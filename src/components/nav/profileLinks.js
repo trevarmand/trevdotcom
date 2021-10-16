5bc864a6170ca0461f21dca6583dda7ffa60767a
trevarmand <trevarmand@protonmail.com>
2021-10-15 23:45:36 -0700

    Add strava as a profile link

--- src/components/nav/profileLinks.js
+++ src/components/nav/profileLinks.js
@@ -2,17 +2,21 @@ import './profileLinks.css'
 import React from 'react';
 import github from '../../images/logos/github.png'
 import linkedin from '../../images/logos/linkedin.png'
+import strava from '../../images/logos/strava.png'
 
 class ProfileLinks extends React.Component {
 
     render() {
         return (
-            <div className='profile-links'>
+            <div className='profile-links mb-3 mt-3'>
                 <a href="https://www.github.com/trevarmand">
-                    <img className='link-image img-spacing' src={github} ></img>
+                    <img className='link-image mr-5' src={github} ></img>
                 </a>
                 <a href="https://linkedin.com/in/trevor-armand">
-                    <img className='link-image' src={linkedin}></img>
+                    <img className='link-image ml-3 mr-5' src={linkedin}></img>
+                </a>
+                <a href="https://strava.com/in/trevor-armand">
+                    <img className='link-image ml-3' src={strava}></img>
                 </a>
             </div>
         )
